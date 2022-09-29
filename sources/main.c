7ddb3fdd59ac7f9c7f7c37786fe1914f5062eae6
Daniel <mrpickles123115@gmail.com>
2022-09-28 21:53:42 -0600

    added asteroids and bullets

--- sources/main.c
+++ sources/main.c
@@ -15,6 +15,12 @@ typedef struct Bullet{
     Vector2 speed;
     bool active;
 } Bullet;
+typedef struct Asteroid{
+    int radius;
+    Vector2 position;
+    Vector2 speed;
+    bool active;
+} Asteroid;
 
 typedef struct BulletNode{
     Bullet bullet;
@@ -23,23 +29,32 @@ typedef struct BulletNode{
 
 void remove_middle_node(BulletNode*, BulletNode*);
 
-const int screenWidth = 650;
-const int screenHeight = 800;
+const int ScreenWidth = 650;
+const int ScreenHeight = 800;
+const int MAX_ASTEROIDS = 15;
+const double ASTEROID_ACCEL = 0.0001;
 
 static Player player;
 BulletNode *FirstBullet = NULL;
 BulletNode *next = NULL; //for iterating
-
+BulletNode *prev = NULL; //also for iterating
+Asteroid Asteroids[MAX_ASTEROIDS] = { 0 };
 
 int main(void)
 {
-    player.position = (Vector2) {screenWidth / 2, screenHeight * 7/8};
+    player.position = (Vector2) {ScreenWidth / 2, ScreenHeight * 7/8};
     player.size = (Vector2) {25, 10};
     player.speed = (Vector2) {5, 0};
 
-    InitWindow(screenWidth, screenHeight, WINDOW_TITLE);
+    for (int i = 0; i < MAX_ASTEROIDS; i++)
+    {
+        Asteroids[i].radius = GetRandomValue(5, 15);
+        Asteroids[i].active = false;
+    }
     SetConfigFlags(FLAG_VSYNC_HINT);
-    SetTargetFPS(60);
+    InitWindow(ScreenWidth, ScreenHeight, WINDOW_TITLE);
+
+    // SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         //Update
@@ -51,8 +66,12 @@ int main(void)
             FirstBullet = malloc(sizeof(BulletNode));
             FirstBullet->next = OldFirst;
             FirstBullet->bullet.position = player.position;
+            //center bullet onto player
             FirstBullet->bullet.size = (Vector2) {5, 10};
             FirstBullet->bullet.speed = (Vector2) {0, -5};
+            FirstBullet->bullet.active = true;
+            FirstBullet->bullet.position.x += (player.size.x - FirstBullet->bullet.size.x) / 2;
+
         }
         if (IsKeyPressed(KEY_C)) {
             while (FirstBullet) {
@@ -61,17 +80,71 @@ int main(void)
                 free(temp);
     }
         }
+        for (int i = 0; i < MAX_ASTEROIDS; i++)
+        {
+            if (Asteroids[i].active) {
+                Asteroids[i].position.x += Asteroids[i].speed.x;
+                Asteroids[i].position.y += Asteroids[i].speed.y;
+                Asteroids[i].speed.y += Asteroids[i].speed.y * ASTEROID_ACCEL;
+                if ((Asteroids[i].position.y >= ScreenHeight) || (Asteroids[i].position.x <= 0) || (Asteroids[i].position.x >= ScreenWidth))
+                {
+                    Asteroids[i].active = false;
+                }
+            }
+        }
 
         next = FirstBullet;
-        BulletNode *prev = FirstBullet;
-        bool firstnode = true;
         while (next) {
             BulletNode *tmp = next;
             next = tmp->next;
-            tmp->bullet.position.x = tmp->bullet.position.x + tmp->bullet.speed.x;
-            tmp->bullet.position.y = tmp->bullet.position.y + tmp->bullet.speed.y;
-            if ((tmp->bullet.position.y <= 20) || (tmp->bullet.position.y >= screenHeight))
+            if (tmp->bullet.active)
             {
+                tmp->bullet.position.x = tmp->bullet.position.x + tmp->bullet.speed.x;
+                tmp->bullet.position.y = tmp->bullet.position.y + tmp->bullet.speed.y;
+                for (int i = 0; i < MAX_ASTEROIDS; i++)
+                {
+                    if (Asteroids[i].active)
+                    {
+                        // Rectangle bullet_info = {tmp->bullet.size.y, tmp->bullet.size.x, tmp->bullet.position.x, tmp->bullet.position.y};
+                        Rectangle bullet_info = {tmp->bullet.position.x, tmp->bullet.position.y, tmp->bullet.size.x, tmp->bullet.size.y};
+                        bool hit = CheckCollisionCircleRec(Asteroids[i].position, Asteroids[i].radius, bullet_info);
+                        if (hit)
+                        {
+                            // tmp->bullet.active = false;
+                            Asteroids[i].active = false;
+                        }
+                    }
+                }
+                if ((tmp->bullet.position.y <= 20) || (tmp->bullet.position.y >= ScreenHeight))
+                {
+                    tmp->bullet.active = false;
+                }
+            }
+        }
+        //-------------------------------
+        //Respawn and removal logic
+        for (int i = 0; i < MAX_ASTEROIDS; i++)
+        {
+            if (!Asteroids[i].active)
+            {
+                Asteroids[i].position.x = GetRandomValue(ScreenWidth * 0.25, ScreenWidth * 0.75);
+                Asteroids[i].position.y = -30;
+                Asteroids[i].speed.x = GetRandomValue(-1, 1);
+                Asteroids[i].radius = GetRandomValue(5, 15);
+                Asteroids[i].speed.y = GetRandomValue(1, 4);
+                Asteroids[i].active = true;
+            }
+        }
+
+
+        next = FirstBullet;
+        prev = FirstBullet;
+        bool firstnode = true;
+        while (next) 
+        {
+            BulletNode *tmp = next;
+            next = tmp-> next;
+            if (tmp->bullet.active == false) {
                 if (firstnode)
                 {
                     FirstBullet = next;
@@ -81,8 +154,9 @@ int main(void)
                 {
                     remove_middle_node(tmp, prev);
                 }
+                
             }
-            if (!firstnode) 
+            if (!firstnode)
             {
                 prev = prev->next;
             }
@@ -96,14 +170,24 @@ int main(void)
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawRectangleV(player.position, player.size, SKYBLUE);
+
+        for (int i = 0; i < MAX_ASTEROIDS; i++)
+        {
+            if (Asteroids[i].active)
+            {
+                DrawCircleV(Asteroids[i].position, Asteroids[i].radius, BLUE);
+            }
+        }
+
         next = FirstBullet;
         while (next) {
             BulletNode *tmp = next;
             next = tmp->next;
             DrawRectangleV(tmp->bullet.position, tmp->bullet.size, SKYBLUE);
         }
-        DrawFPS(10, 10);
 
+        DrawRectangle(0, 0, 10, 20, BLUE);
+        DrawFPS(10, 10);
         EndDrawing();
         //------------------------------------------------------------------------------------------------------------------
     }
